e28867c8d61217a193782f7254984676ca56c1f8
Henrique Costa <henrique.costa@ibm.com>
2022-06-27 11:08:37 -0300

    Initial commit

---
+++ src/test/java/com/henrique/costa/projeto_final/ProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.costa.projeto_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
