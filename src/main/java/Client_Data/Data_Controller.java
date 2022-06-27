06542cdf6e95e2e546377749a32dd9331a8de012
Henrique Costa <henrique.costa@ibm.com>
2022-06-27 11:40:17 -0300

    Nova classe dados e modificacoes em data controller

--- src/main/java/Client_Data/Data_Controller.java
+++ src/main/java/Client_Data/Data_Controller.java
@@ -1,15 +1,23 @@
 package Client_Data;
 
+import Modelo_de_dados.Dados;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/dados_produto")
+@SpringBootApplication
 public class Data_Controller {
 
     @GetMapping
-    public String hello(){
-        return "hello, world";
+    public List<Dados> List(){
+    }
+    public static void main(String[] args){
+        SpringApplication.run(Data_Controller.class, args);
     }
 }
---
+++ src/main/java/Modelo_de_dados/Dados.java
@@ -0,0 +1,6 @@
+package Modelo_de_dados;
+
+public class Dados {
+    private Long id;
+    private String nome;
+}
