8d5c6963eea5b12cdfac2f52fd75b26051578562
Henrique Costa <henrique.costa@ibm.com>
2022-06-27 11:51:24 -0300

    Adicionando banco de dados no pom

--- src/main/java/Modelo_de_dados/Dados.java
+++ src/main/java/Modelo_de_dados/Dados.java
@@ -1,6 +1,17 @@
 package Modelo_de_dados;
 
+import Client_Data.Data_Controller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Dados {
     private Long id;
     private String nome;
+
+    public static void main(String[] args){
+        SpringApplication.run(Dados.class, args);
+    }
 }
+}
+
